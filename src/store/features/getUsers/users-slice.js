105d6cd571754faf1d804c6b35f04cc910d889dd
Vladislav <vladgm233223@gmail.com>
2023-02-05 12:02:22 +0200

    Create users-slice.js

---
+++ src/store/features/getUsers/users-slice.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const loadUsers = createAsyncThunk(
+  "@@users/load-users",
+  (api, { extra: { client} }) => {
+    return client.get(api);
+  }
+);
+
+const initialState = {
+  status: "idle",
+  error: null,
+  list: [],
+};
+
+const userSlice = createSlice({
+  name: "@@users",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadUsers.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(loadUsers.rejected, (state, action) => {
+        state.status = "rejected";
+        state.error = action.payload || action.meta.error;
+      })
+      .addCase(loadUsers.fulfilled, (state, action) => {
+        state.status = "received";
+        state.list = action.payload.data.users;
+      });
+  },
+});
+
+export const usersReducer = userSlice.reducer;
+
+//selectors
+
+export const selectUsersInfo = (state) => ({
+  status: state.users.status,
+  error: state.users.error,
+  qty: state.users.list.length,
+});
+
+export const selectUsers = (state) => state.users.list;
