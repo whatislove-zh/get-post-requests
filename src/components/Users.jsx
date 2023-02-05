267bd2891642b936ae0b2f9be27ba54daa2ae301
Vladislav <vladgm233223@gmail.com>
2023-02-05 12:03:15 +0200

    show users from api

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,87 +3,31 @@ import { useState } from "react";
 
 import { UserCard } from "./UserCard";
 import { StyledButton } from "./StyledButton";
+import { useDispatch, useSelector } from "react-redux";
+import { setExpand } from "../store/features/expand/expand-slice";
+import { useUsers } from "../store/features/getUsers/use-users";
 
-const USERS_DATA = {
-  users: [
-    {
-      id: 11861,
-      name: "123",
-      email: "1231231@12312312.12312",
-      phone: "380661155555",
-      position: "Lawyer",
-      position_id: 1,
-      registration_timestamp: 1675470252,
-      photo:
-        "https://frontend-test-assignment-api.abz.agency/images/users/63dda5acacee711861.jpg",
-    },
-    {
-      id: 11860,
-      name: "1231231",
-      email: "1231323@12312.123",
-      phone: "380662312312",
-      position: "Lawyer",
-      position_id: 1,
-      registration_timestamp: 1675468240,
-      photo:
-        "https://frontend-test-assignment-api.abz.agency/images/users/63dd9dd0c540411860.jpg",
-    },
-    {
-      id: 11859,
-      name: "12312",
-      email: "12312@12.12312312",
-      phone: "380508888888",
-      position: "Lawyer",
-      position_id: 1,
-      registration_timestamp: 1675468007,
-      photo:
-        "https://frontend-test-assignment-api.abz.agency/images/users/63dd9ce71024411859.jpg",
-    },
-    {
-      id: 1,
-      name: "Leanne West",
-      email: "onie34@lubowitz.com",
-      phone: "+380936050764",
-      position: "Content manager",
-      position_id: 2,
-      registration_timestamp: 1604494937,
-      photo:
-        "https://frontend-test-assignment-api.abz.agency/images/users/5fa2a6596d3bb1.jpeg",
-    },
-    {
-      id: 2,
-      name: "Ahmad Rodriguez",
-      email: "isadore08@zulauf.biz",
-      phone: "+380993215621",
-      position: "Security",
-      position_id: 3,
-      registration_timestamp: 1604494937,
-      photo:
-        "https://frontend-test-assignment-api.abz.agency/images/users/5fa2a6596f0072.jpeg",
-    },
-    {
-      id: 3,
-      name: "Jeromy Schultz",
-      email: "gladys74@emmerich.com",
-      phone: "+380957332233",
-      position: "Security",
-      position_id: 3,
-      registration_timestamp: 1604494937,
-      photo:
-        "https://frontend-test-assignment-api.abz.agency/images/users/5fa2a659709143.jpeg",
-    },
-  ],
-};
 
 export const Users = () => {
-const [users/*, setUsers*/ ] = useState(USERS_DATA.users);
+  const [users, { error, status }] = useUsers();
 
   const [numberOfitemsShown, setNumberOfItemsToShown] = useState(6);
 
-  const showMore = () => {
-    setNumberOfItemsToShown((prev) => prev + 3);
-  };
+  const dispatch = useDispatch();
 
+  const isExpand = useSelector((state) => state.expand);
+
+  const showMore = () => {
+    if (numberOfitemsShown + 6 <= users.length) {
+      setNumberOfItemsToShown(numberOfitemsShown + 6);
+      
+    } else {
+      setNumberOfItemsToShown(users.length);
+      dispatch(setExpand(false));
+      
+    }
+  }
+  
   return (
     <Box sx={{ mb: "140px" }} align="center">
       <Typography
@@ -95,20 +39,28 @@ const [users/*, setUsers*/ ] = useState(USERS_DATA.users);
       >
         Working with GET request
       </Typography>
-      <Grid container spacing="29" align="center" sx={{ mb: "50px" }}>
-        {users.slice(0, numberOfitemsShown).map((user) => (
-          <Grid item lg={4} sm={6} xs={12} key={user.id}>
-            <UserCard user={user} />
-          </Grid>
-        ))}
-      </Grid>
+      {error && <Typography>{error.message}</Typography>}
+      {status === "loading" && <Typography>Loading</Typography>}
+      {status === "received" && (
+        <Grid container spacing="29" align="center" sx={{ mb: "50px" }}>
+          {users.slice(0, numberOfitemsShown).map((user) => (
+            <Grid item lg={4} sm={6} xs={12} key={user.id}>
+              <UserCard user={user} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
 
-      <StyledButton
-        title="Show more"
-        onClick={() => {
-          showMore();
-        }}
-      />
+      {isExpand ? (
+        <StyledButton
+          title="Show more"
+          onClick={() => {
+            showMore();
+          }}
+        />
+      ) : (
+        <StyledButton title="No more users" disabled />
+      )}
     </Box>
   );
 };
